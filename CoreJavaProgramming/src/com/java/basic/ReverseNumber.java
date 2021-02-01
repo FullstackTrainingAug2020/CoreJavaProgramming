67a83fc025598f7ec4215979104b0400a1b54124
Vamshi <svamshikiran@gmail.com>
2021-02-01 22:51:28 +0530

    Methods examples

--- CoreJavaProgramming/src/com/java/basic/ReverseNumber.java
+++ CoreJavaProgramming/src/com/java/basic/ReverseNumber.java
@@ -4,20 +4,34 @@ public class ReverseNumber {
 
 	public static void main(String[] args) {
 		int number = 1234;
-		
+		System.out.println("REVERSE NUMBER: " + reverse(number));
+		System.out.println("SUM OF DIGITS OF A NUMBER: " + sumOfDigits(number));
+	}
+
+	private static int reverse(int number) {
 		int temp;
-		
-		int reverseNumber = 0; 
-		
+		int reverseNumber = 0;
+
 		while (number > 0) {
-			temp = number%10;
-			
+			temp = number % 10;
 			reverseNumber = reverseNumber * 10 + temp;
-			
-			number = number/10;
+			number = number / 10;
 		}
-		
-		System.out.println("REVERSE NUMBER: "+reverseNumber);
+
+		return reverseNumber;
+	}
+
+	private static int sumOfDigits(int number) {
+		int temp;
+		int sum = 0;
+
+		while (number > 0) {
+			temp = number % 10;
+			sum = sum + temp;
+			number = number / 10;
+		}
+
+		return sum;
 	}
 
 }
